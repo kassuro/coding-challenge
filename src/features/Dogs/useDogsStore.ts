35039666bef94c360517d031a9a12cc0dc725670
Kassuro <l.fuhl@demv.de>
2022-10-18 18:12:49 +0200

    feat(DogStore): implement filtered dog list as getter in store

--- src/features/Dogs/useDogsStore.ts
+++ src/features/Dogs/useDogsStore.ts
@@ -5,11 +5,13 @@ import { useDogApi } from '@/features/Dogs/useDogApi';
 
 interface DogStoreState {
   data: RandomDogImages['message'];
+  filterValue: string;
 }
 
 export const useDogsStore = defineStore('DogStore', {
   state: (): DogStoreState => ({
     data: [],
+    filterValue: '',
   }),
 
   actions: {
@@ -19,6 +21,16 @@ export const useDogsStore = defineStore('DogStore', {
       const result = await loadImages();
 
       this.data = result.data.value.message;
+    },
+  },
+
+  getters: {
+    filteredImageLinks(state) {
+      return state.data.filter((imgUrl) => {
+        const breed = imgUrl.split('breeds/')[1].split('/')[0].toLowerCase();
+
+        return breed.includes(state.filterValue.toLowerCase());
+      });
     },
   },
 });
