a3c009b6ced642178c945537b93c69dbd186706c
Kassuro <l.fuhl@demv.de>
2022-10-18 18:13:08 +0200

    feat(Dog ListView): build simple dog list view simpe filtering

---
+++ src/features/Dogs/components/ListView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="p-3">
+    <div class="ml-3">
+      <label
+        class="mb-1 block text-gray-500 hover:cursor-pointer"
+        for="breed-filter"
+        >Filter images by dog breed:
+      </label>
+      <TextInput
+        placeholder="Please enter a dog breed"
+        v-model="dogStore.filterValue"
+        id="breed-filter"
+      />
+    </div>
+
+    <ImageList :data="dogStore.filteredImageLinks">
+      <template v-slot="{ item }">
+        <ImageListItem :src="(item as string) " alt="A picture of a dog" />
+      </template>
+    </ImageList>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useDogsStore } from '@/features/Dogs/useDogsStore';
+
+const dogStore = useDogsStore();
+dogStore.loadImages();
+</script>
