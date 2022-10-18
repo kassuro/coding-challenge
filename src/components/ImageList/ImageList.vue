ba10d683f0045c38ddca62eacf1342bab66fd6e2
Kassuro <l.fuhl@demv.de>
2022-10-18 18:11:03 +0200

    feat(ImgList): add a image list and listitem component

---
+++ src/components/ImageList/ImageList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+    <template v-for="(item, i) in data" :key="i">
+      <slot :item="item" />
+    </template>
+  </div>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+  data: unknown[];
+}
+
+defineProps<Props>();
+</script>
---
+++ src/components/ImageList/ImageListItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+    class="w-30 h-30 p-3 flex justify-center items-center border rounded-md shadow-md hover:shadow-gray-300 hover:shadow-lg hover:cursor-pointer m-3"
+  >
+    <img class="object-contain" :src="src" :alt="alt" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+  src: string;
+  alt: string;
+}
+
+defineProps<Props>();
+</script>
