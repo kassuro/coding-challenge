c985202535cad480589877086d52b0d7afffc474
Kassuro <l.fuhl@demv.de>
2022-10-19 17:43:56 +0200

    feat(List comp): show error if no data could be loaded

--- src/components/ImageList/ImageList.vue
+++ src/components/ImageList/ImageList.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+  <div
+    v-if="error"
+    class="flex-col border border-red-500 text-red-700 rounded px-5 py-3 my-3"
+  >
+    <h2 class="text-lg">Error!</h2>
+    <p>No data could be found!</p>
+  </div>
+
+  <div v-else class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
     <template v-for="(item, i) in data" :key="i">
       <slot :item="item" />
     </template>
@@ -8,8 +16,20 @@
 
 <script lang="ts" setup>
 interface Props {
-  data: unknown[];
+  data: unknown[] | undefined | null;
 }
 
-defineProps<Props>();
+const props = defineProps<Props>();
+
+const error = ref(false);
+
+if (props.data === undefined || props.data === null) {
+  /**
+   * nuxt seems to abstract the error handling for the request
+   * so we just get no data and an error flag.
+   * We abuse this a little for the sake of simplicity and say it's an error
+   * if we get not data into the list component.
+   */
+  error.value = true;
+}
 </script>
